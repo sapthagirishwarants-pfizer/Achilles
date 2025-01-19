4f0e244375643caedace43b27c52063f06520fbc
sapthagirishwarants-pfizer <sapthagirishwaran.thennalsivaramakrishnan@pfizer.com>
2025-01-19 14:17:50 -0800

    fix snowflake: updated cast to try_cast

--- inst/sql/sql_server/export/provider/sqlProviderSpecialty.sql
+++ inst/sql/sql_server/export/provider/sqlProviderSpecialty.sql
@@ -4,6 +4,6 @@ select c.concept_id     as concept_id,
        1.0*count_value/sum(count_value)over() as percent_persons
  from @results_database_schema.achilles_results ar
  join @vocab_database_schema.concept c
-   on ar.stratum_1 = cast(c.concept_id as varchar) 
+   on ar.stratum_1 = TRY_CAST(c.concept_id as varchar) 
 where ar.analysis_id = 301
   and c.concept_id != 0; 
